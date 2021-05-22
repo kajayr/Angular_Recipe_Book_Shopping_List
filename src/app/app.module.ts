5f7fc564ac962ab4d2f884396af24e1f17d9a9f7
Leo Sinani <kajayr_sinanian@yahoo.com>
2021-05-21 22:24:01 -0700

    Starting the angular project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
+import { ShoperList } from './../root/Header/Shopper_list/shopper_list.component';
+import { RootComponent } from './../root/root.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {Header} from '../root/Header/header.component';
 import { AppComponent } from './app.component';
+import { RecipeComponent } from '../root/Header/recipe/recipe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RootComponent,
+    Header,
+    ShoperList,
+    RecipeComponent
+
   ],
   imports: [
     BrowserModule
---
+++ src/root/Header/Shopper_list/shopper_list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'shopper_list-app',
+    templateUrl: './shopper_list.component.html',
+    styleUrls: ['./shopper_list.component.css']
+})
+
+export class ShoperList{
+
+}
